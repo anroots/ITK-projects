fe405efa45dc32656820dfc716d10577981b5ff3
Ando Roots <andoroots+signup@gmail.com>
2011-11-23 14:50:57 +0200

    Working switch

--- Digital Logic/Lab 2/Behavior_Switch.vhd
+++ Digital Logic/Lab 2/Behavior_Switch.vhd
@@ -1,1 +1,46 @@
---todo
+entity FullAdderSwitch is
+  port ( a, b, c_in : in bit;
+s, c_out : out bit );
+end FullAdderSwitch;
+ 
+architecture behave of FullAdderSwitch is
+
+
+begin
+  process ( a, b, c_in )
+  
+	variable inputs: bit_vector (2 downto 0);
+  
+	begin
+  
+	inputs := a & b & c_in;
+  
+  	case inputs is 
+   	when "001" =>
+   		s <= '1';
+		c_out <= '0';
+	when "010" =>
+		s <= '1';
+		c_out <= '0';
+	when "011" =>
+		s <= '0';
+		c_out <= '1';
+	when "100" =>
+		s <= '1';
+		c_out <= '0';
+	when "101" =>
+		s <= '0';
+		c_out <= '1';
+	when "110" =>
+		s <= '0';
+		c_out <= '1';
+	when "111" =>
+		s <= '1';
+		c_out <= '1';
+	when "000" =>
+	s <= '0';
+	c_out <= '0';
+   end case;
+  
+  end process;
+end behave;
