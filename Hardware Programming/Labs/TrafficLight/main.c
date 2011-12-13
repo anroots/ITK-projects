0e71ae6e19b7da39efd27b4b4ece8a77f0a24537
Ando Roots <andoroots@gmail.com>
2011-12-13 22:57:33 +0200

    Added base project files for TrafficLight

---
+++ Hardware Programming/Labs/TrafficLight/main.c
@@ -0,0 +1,31 @@
+#include <8051.h>
+
+/**
+ * Subject: Hardware Programming 2011
+ * Lab 9: Traffic light
+ * Team members:
+ * 	Ando Roots
+ * 	Eero Jämsä
+ *	Marek Kikkas
+**/
+
+
+#define LIGHTS P1
+
+/**
+ * Configure startup environment
+**/
+void init() {
+
+}
+
+/**
+ * Main superloop function
+**/
+void main() {
+	init();
+
+	while(1) {
+		
+	}
+}
