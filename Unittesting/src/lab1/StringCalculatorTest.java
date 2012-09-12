aceee5ca4a7227870e84a95f12247c084edcb0ae
Ando Roots <ando@roots.ee>
2012-09-12 16:16:55 +0300

    Fix annotation error.

--- Unittesting/src/lab1/StringCalculatorTest.java
+++ Unittesting/src/lab1/StringCalculatorTest.java
@@ -8,33 +8,33 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest {
 
 	StringCalculator str;
-	
+
 	@Before
-	public void setUp(){
-		StringCalculator str = new StringCalculator();
+	public void setUp() {
+		str = new StringCalculator();
 	}
-	
-	
-	public void add_emptyString_returnsZero(){
-		assertThat(str.add(""),is(0));
+
+	@Test
+	public void add_emptyString_returnsZero() {
+		assertThat(str.add(""), is(0));
 	}
-	
-	public void add_singleNumber_returnsSingleNumber(){
-		assertThat(str.add("-854"),is(-854));
-		assertThat(str.add("0"),is(0));
-		assertThat(str.add("751"),is(751));
+
+	@Test
+	public void add_singleNumber_returnsSingleNumber() {
+		assertThat(str.add("-854"), is(-854));
+		assertThat(str.add("0"), is(0));
+		assertThat(str.add("751"), is(751));
 	}
-	
-	public void add_twoNumbers_returnsSum(){
-		assertThat(str.add("-95, -2"),is(-97));
-		assertThat(str.add("-95, 95"),is(0));
-		assertThat(str.add("7, 2"),is(9));
+
+	@Test
+	public void add_twoNumbers_returnsSum() {
+		assertThat(str.add("-95, -2"), is(-97));
+		assertThat(str.add("-95, 95"), is(0));
+		assertThat(str.add("7, 2"), is(9));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void add_null_throws() throws IllegalArgumentException {
-		str = new StringCalculator();
 		str.add(null);
 	}
-
 }
