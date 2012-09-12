91c3cabf33c64e374f12d41fa9b2dacb97e0a95d
Ando Roots <ando@roots.ee>
2012-09-12 16:52:29 +0300

    Finished Stack assignment

--- Unittesting/src/ex/s1/Stack.java
+++ Unittesting/src/ex/s1/Stack.java
@@ -1,21 +1,60 @@
 package ex.s1;
 
+/**
+ * 
+ * @author Ando Roots
+ *
+ */
 public class Stack {
-	
-	
+
+	/**
+	 * Justification for using a fixed-size stack: assignment did not allow the
+	 * usage of Lists and did not specify that the Stack must support infinite
+	 * members.
+	 */
+	private Integer dataStructure[] = new Integer[100];
+
+	/**
+	 * Essentially the length of the array
+	 */
+	private int topIndex;
+
+	/**
+	 * Add element
+	 * @param i
+	 */
 	public void push(Integer i) {
-		
+		dataStructure[++topIndex] = i;
 	}
 
+	/**
+	 * Remove element
+	 * @return
+	 */
 	public Integer pop() {
-		return 0;
+		if (topIndex == 0) {
+			throw new IllegalStateException();
+		}
+		topIndex--;
+		return dataStructure[topIndex + 1];
 	}
 
+	/**
+	 * Look at topmost element, do not remove
+	 * @return
+	 */
 	public Integer peek() {
-		return 0;
+		if (topIndex == 0) {
+			throw new IllegalStateException();
+		}
+		return dataStructure[topIndex];
 	}
 
+	/**
+	 * Get current size
+	 * @return
+	 */
 	public Integer size() {
-		return 0;
+		return topIndex;
 	}
 }
--- Unittesting/src/ex/s1/StackTest.java
+++ Unittesting/src/ex/s1/StackTest.java
@@ -1,10 +1,16 @@
 package ex.s1;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+/**
+ * 
+ * @author Ando Roots
+ *
+ */
 public class StackTest {
 
 	private Stack stack;
@@ -17,5 +23,73 @@ public class StackTest {
 	@Test
 	public void create_stack_isEmpty() {
 		assertThat(stack.size(), is(0));
+	}
+
+	@Test
+	public void push_two_containsTwo() {
+		stack.push(3);
+		stack.push(5);
+		assertThat(stack.size(), is(2));
+	}
+
+	@Test
+	public void push_twoRemoveTwo_containsZero() {
+		stack.push(3);
+		stack.push(5);
+		stack.pop();
+		stack.pop();
+		assertThat(stack.size(), is(0));
+	}
+
+	@Test
+	public void push_twoRemoveTwo_elementsMatch() {
+		int one = 5;
+		int two = -8;
+		stack.push(one);
+		stack.push(two);
+
+		assertThat(stack.pop(), is(two));
+		assertThat(stack.pop(), is(one));
+	}
+
+	@Test
+	public void push_two_topmostMatches() {
+		stack.push(6);
+		stack.push(87);
+		assertThat(stack.peek(), is(87));
+	}
+
+	@Test
+	public void push_twoAndPeek_containsTwo() {
+		stack.push(-76);
+		stack.push(874);
+		assertThat(stack.peek(), is(874));
+		assertThat(stack.size(), is(2));
+	}
+
+	@Test
+	public void push_twoPeekTwice_peeksMatch() {
+		stack.push(0);
+		stack.push(104);
+
+		int peek1 = stack.peek();
+		int peek2 = stack.peek();
+
+		assertThat(peek1, is(peek2));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void create_tryPop_throws() {
+		stack.pop();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void create_tyPeek_throws() {
+		stack.peek();
+	}
+
+	@After
+	public void after() {
+		stack = new Stack();
 	}
 }
